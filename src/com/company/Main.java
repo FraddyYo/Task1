0c9e582a9e625ee846a6bcadb2a42f11b1e2969e
Georgii <loveOlechka@mail.ru>
2021-12-22 15:17:08 +0300

    Сделана задача 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        byte a = 5;
+        System.out.println(a);
+        short b = 150;
+        System.out.println(b);
+        int c = 15;
+        System.out.println(c);
+        long d = 4000000000L;
+        System.out.println(d);
+        float e = 45.55F;
+        System.out.println(e);
+        double f = 0.88888888888;
+        System.out.println(f);
     }
 }
